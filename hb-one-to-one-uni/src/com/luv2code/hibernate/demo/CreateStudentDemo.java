a8e2a7a01dd14309607e722a4b2b73e678caac72
Kamil <bolish11@gmail.com>
2019-06-25 17:45:35 +0200

    1-1 DB connection
    one to one uni connection hibernate annotation

---
+++ hb-one-to-one-uni/src/com/luv2code/hibernate/demo/CreateStudentDemo.java
@@ -0,0 +1,39 @@
+package com.luv2code.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class CreateStudentDemo {
+    public static void main(String[] args){
+
+        //create session factory
+        SessionFactory factory = new Configuration()
+                                 .configure("hibernate.cfg.xml")
+                                 .addAnnotatedClass(Student.class)
+                                 .buildSessionFactory();
+        //create session
+        Session session = factory.getCurrentSession();
+
+        try{
+            //use the session object to save Java object
+            //create Student object
+            System.out.println("Creating new student...");
+            Student tempStudent = new Student("Paul","Wall","paul@luv2code.com");
+
+            //Start/begin transaction
+            session.beginTransaction();
+
+            //Save student object
+            System.out.println("Saving the student...");
+            session.save(tempStudent);
+
+            //commint transaction
+            session.getTransaction().commit();
+            System.out.println("Commit done.");
+
+        }finally {
+            factory.close();
+        }
+    }
+}
---
+++ hb-one-to-one-uni/src/com/luv2code/hibernate/demo/entity/Instructor.java
@@ -0,0 +1,26 @@
+package com.luv2code.hibernate.demo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "instructor")
+public class Instructor {
+
+    @Id
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    private int instructorDetailId;
+}
